54f503c74538d9f0f20ec61ddaf96e606e28b30c
Gabriel ML <gabriellevineml@gmail.com>
2020-04-04 17:31:50 -0400

    playing around with styling the header also added test logo image

--- header.php
+++ header.php
@@ -1,5 +1,8 @@
 <?php
-echo "<div style=\"text-align:center;\">";
+echo "<div id=\"header\">";
+echo "<img id=\"logo\" src=\"img/Logo_GroupOne.png\"/>";
 echo "<a href=\"index.php\">Main</a>";
+echo "<a href=\"blank.php\">Blank</a>";
+echo "<a href=\"blank.php\">Blank</a>";
 echo "</div>"
 ?>
