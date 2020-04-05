2a10657c7f81d25dddd36d5195c8828fd8879308
Michael Plath <mikeaplath@gmail.com>
2020-04-05 01:14:55 -0400

    search results returned to content area

--- handle_search.php
+++ handle_search.php
@@ -2,6 +2,11 @@
 
 
 
+$items = get_items();
+$result_count = mysqli_num_rows($items);
+include('index.php');
+
+
 function connect_to_database(){
 
 	$host = "localhost";
@@ -9,25 +14,31 @@ function connect_to_database(){
 	$database_password = "granfalloon";
 	$database_name = "csc350project";
 
-
 	$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
 		die("Unable to connect to MYSQL server:".mysqli_connect_error());
 		return $dbc;
 }
 
-function query_items($dbc){
-		$price = $_REQUEST['priceslider'];
+function query_items($dbc,$price){
+		
 		$query = "SELECT * from items WHERE price<=".$price."";
 		//echo $query;
 		$result = mysqli_query($dbc,$query);
-		echo "<br>Results:".$result->num_rows."<br>";
+		//echo "<br>Results:".$result->num_rows."<br>";
 		//$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		while($row = $result->fetch_assoc()){
-			echo $row['name'].' '.$row['price'].'<br>'.$row['description'].'<br>';
-		}
+		//while($row = $result->fetch_assoc()){
+		//	echo $row['name'].' '.$row['price'].'<br>'.$row['description'].'<br>';
+		//}
+		return $result;
 }
 
+function get_items(){
+		$price = $_REQUEST['priceslider'];
 		$dbc  = connect_to_database();
-		query_items($dbc);
+		$result = query_items($dbc,$price);
+		return $result;
+}
+
+	
 
 ?>
