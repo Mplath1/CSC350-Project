4673a7bccd96bebadb9584f8bdba2437da3b0425
Michael Plath <mikeaplath@gmail.com>
2020-04-04 22:44:20 -0400

    rewrote code as functions

--- handle_search.php
+++ handle_search.php
@@ -1,26 +1,33 @@
 <?php
 
-$price = $_REQUEST['priceslider'];
 
-$host = "localhost";
-$user = "devgroup";
-$database_password = "granfalloon";
-$database_name = "csc350project";
 
-$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
+function connect_to_database(){
+
+	$host = "localhost";
+	$user = "devgroup";
+	$database_password = "granfalloon";
+	$database_name = "csc350project";
+
+
+	$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
 		die("Unable to connect to MYSQL server:".mysqli_connect_error());
+		return $dbc;
+}
 
+function query_items($dbc){
+		$price = $_REQUEST['priceslider'];
 		$query = "SELECT * from items WHERE price<=".$price."";
-		echo $query;
+		//echo $query;
 		$result = mysqli_query($dbc,$query);
 		echo "<br>Results:".$result->num_rows."<br>";
 		//$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		while($row = $result->fetch_assoc()){
 			echo $row['name'].' '.$row['price'].'<br>'.$row['description'].'<br>';
-			
 		}
+}
 
-
-
+		$dbc  = connect_to_database();
+		query_items($dbc);
 
 ?>
