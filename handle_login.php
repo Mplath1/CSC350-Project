6983337d41f97580ceb8828d7f311dece49e11d2
Michael Plath <mikeaplath@gmail.com>
2020-04-04 21:59:59 -0400

    rewrote code as functions

--- handle_login.php
+++ handle_login.php
@@ -4,19 +4,24 @@ $username = filter_var($_REQUEST['username'],FILTER_SANITIZE_STRING);
 $password = filter_var($_REQUEST['password'],FILTER_SANITIZE_STRING);
 $msg = "";
 
-$host = "localhost";
-$user = "devgroup";
-$database_password = "granfalloon";
-$database_name = "csc350project";
 
 
-if(!empty($username)){
-	if(!empty($password)){
-		//this should be written as seperate functions which are called here
-		$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
+function connect_to_database(){
+
+	$host = "localhost";
+	$user = "devgroup";
+	$database_password = "granfalloon";
+	$database_name = "csc350project";
+
+
+	$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
 		die("Unable to connect to MYSQL server:".mysqli_connect_error());
+		return $dbc;
+}
 
-		$query = "SELECT * from users WHERE username='".$username."'";
+
+function select_user($dbc,$username,$password,$msg){
+	$query = "SELECT * from users WHERE username='".$username."'";
 		$result = mysqli_query($dbc,$query);
 	
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -24,10 +29,17 @@ if(!empty($username)){
 		if($row['password'] == sha1($password)){
 			$msg = 'Successful login of user:'.$username.'';
 			header("Location: index.php"); //transfers page to index. need to store username for unique privledges here
+			exit;
 		}else{
-			$msg = "Incorrect Password! ";
+			 $msg = "Incorrect Password! ";
+			 return $msg;
 		}
-		query();
+}
+
+if(!empty($username)){ 
+	if(!empty($password)){
+		$dbc = connect_to_database();
+		$msg=select_user($dbc,$username,$password,$msg);
 	}else{
 		$msg = "You must enter a Password!";
 	}
