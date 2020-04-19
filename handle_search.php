357d95813b2cdeaad8bea39d7ac9b7426dd839d7
Michael Plath <mikeaplath@gmail.com>
2020-04-19 19:55:37 -0400

    flexbox for items.image placeholder

--- handle_search.php
+++ handle_search.php
@@ -14,21 +14,15 @@ function connect_to_database(){
 	$database_password = "granfalloon";
 	$database_name = "csc350project";
 
-	$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect 
+	$dbc  = @mysqli_connect($host,$user,$database_password,$database_name) OR //function to connect
 		die("Unable to connect to MYSQL server:".mysqli_connect_error());
 		return $dbc;
 }
 
 function query_items($dbc,$price){
-		
+
 		$query = "SELECT * from items WHERE price<=".$price."";
-		//echo $query;
 		$result = mysqli_query($dbc,$query);
-		//echo "<br>Results:".$result->num_rows."<br>";
-		//$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-		//while($row = $result->fetch_assoc()){
-		//	echo $row['name'].' '.$row['price'].'<br>'.$row['description'].'<br>';
-		//}
 		return $result;
 }
 
@@ -39,6 +33,6 @@ function get_items(){
 		return $result;
 }
 
-	
+
 
 ?>
--- index.php
+++ index.php
@@ -1,76 +1,83 @@
-<?php 
+<?php
 if(!isset($items)){$items=NULL;}
-if(!isset($result_count)){$result_count=NULL;}	
+if(!isset($result_count)){$result_count=NULL;}
+
 ?>
 <html>
 <head>
 <title>Index</title>
-<link rel="stylesheet" type="text/css" href="styles.css">
+<link rel="stylesheet" type="text/css" href="index_style.css">
 <script src="scripts.js"></script>
 </head>
 <body>
 <?php include 'header.php';?>
-<nav>  <!--possibly use css flex for layouts, but for now here is nav, aside, and article-->
-	<ul>
-		<li><a href="www.somelink.com">Link 1</a></li>
-		<li><a href="www.somelink.com">Link 2</a></li>
-		<li><a href="www.somelink.com">Link 3</a></li>
-	</ul>
-</nav>
-<aside>
-	<form action="handle_search.php" method="get">
-	<ul>
-		<li><p>Option 1 (Dropdown or check box)</p></li>
-		<li><p>Option 2 (Dropdown or check box)</p></li>
-		<li><label for="priceslider">Max.Price</label>
-		<input type="range" name="priceslider" id="priceslider" min="0" max="500" value="50">
-		<br><p>$<span id="currentprice"></span></p></li>
-		<script src="scripts.js"></script>
-	</ul>
-	<input type="submit" value="Search">
-	<form>
-</aside>
-<article>
-	<?php if (empty($items)):?>
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-		labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit. Porta non pulvinar
-		neque laoreet suspendisse interdum. Nunc id cursus metus aliquam eleifend mi in nulla posuere.
-		Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Interdum velit
-		laoreet id donec ultrices. Parturient montes nascetur ridiculus mus mauris. Ornare aenean euismod
-		elementum nisi quis eleifend. Sit amet cursus sit amet dictum sit. Purus ut faucibus pulvinar
-		elementum integer enim neque volutpat ac. Sit amet est placerat in egestas. Arcu felis bibendum
-		ut tristique et egestas quis. Ut diam quam nulla porttitor massa id.<br>Sed libero enim sed faucibus
-		turpis in eu. Quis auctor elit sed vulputate mi sit. Quam vulputate dignissim suspendisse in est
-		ante in. Nam at lectus urna duis. Ut porttitor leo a diam sollicitudin tempor. Adipiscing diam
-		donec adipiscing tristique risus nec feugiat. Urna nec tincidunt praesent semper feugiat. Quisque
-		egestas diam in arcu cursus euismod quis viverra nibh. Nisl pretium fusce id velit ut tortor.
-		Porttitor rhoncus dolor purus non enim praesent elementum. Malesuada pellentesque elit eget gravida
-		cum sociis natoque.<br>Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Purus in
-		mollis nunc sed id semper. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut
-		placerat. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Sed vulputate mi sit amet
-		mauris commodo. Nec ullamcorper sit amet risus nullam. Aenean euismod elementum nisi quis eleifend
-		quam. Morbi non arcu risus quis varius quam quisque. Est velit egestas dui id ornare arcu odio ut
-		sem. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Nibh tortor id
-		aliquet lectus proin nibh nisl condimentum id. In hendrerit gravida rutrum quisque non tellus orci.
-		Vehicula ipsum a arcu cursus. Amet risus nullam eget felis eget nunc lobortis. Enim nunc faucibus
-		a pellentesque. Dignissim convallis aenean et tortor. Imperdiet proin fermentum leo vel orci porta
-		non pulvinar neque.</p>
-	<?php else: ?>
+	<nav>
 		<ul>
-			<p>Items Found:<?=$result_count;?></p>
-			<?php foreach($items as $item): ?>
-				<li>
-				<!--use CSS flex formatting here to create squares-->
-					<h1><?=$item['name'];?></h1>
-					<h2><?=$item['price'];?></h2>
-					<p><?=$item['description'];?><br></p>
-				</li>
-			<?php endforeach;?>
+			<li><a href="www.somelink.com">Link 1</a></li>
+			<li><a href="www.somelink.com">Link 2</a></li>
+			<li><a href="www.somelink.com">Link 3</a></li>
 		</ul>
-	<?php endif;?>
-</article>
+	</nav>
+	<aside>
+		<form action="handle_search.php" method="get">
+			<div class="selection-window">
+				<ul>
+					<li><p>Option 1 (Dropdown or check box)</p></li>
+					<li><p>Option 2 (Dropdown or check box)</p></li>
+					<li><label for="priceslider">Max.Price</label>
+					<input type="range" name="priceslider" id="priceslider" min="0" max="500" value="50">
+					<br><p>$<span id="currentprice"></span></p></li>
+					<script src="scripts.js"></script>
+				</ul>
+					<input type="submit" value="Search">
+				</div>
+	</form>
+	</aside>
+	<article>
+		<?php if (empty($items)):?>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+			labore et dolore magna aliqua. Integer quis auctor elit sed vulputate mi sit. Porta non pulvinar
+			neque laoreet suspendisse interdum. Nunc id cursus metus aliquam eleifend mi in nulla posuere.
+			Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Interdum velit
+			laoreet id donec ultrices. Parturient montes nascetur ridiculus mus mauris. Ornare aenean euismod
+			elementum nisi quis eleifend. Sit amet cursus sit amet dictum sit. Purus ut faucibus pulvinar
+			elementum integer enim neque volutpat ac. Sit amet est placerat in egestas. Arcu felis bibendum
+			ut tristique et egestas quis. Ut diam quam nulla porttitor massa id.<br>Sed libero enim sed faucibus
+			turpis in eu. Quis auctor elit sed vulputate mi sit. Quam vulputate dignissim suspendisse in est
+			ante in. Nam at lectus urna duis. Ut porttitor leo a diam sollicitudin tempor. Adipiscing diam
+			donec adipiscing tristique risus nec feugiat. Urna nec tincidunt praesent semper feugiat. Quisque
+			egestas diam in arcu cursus euismod quis viverra nibh. Nisl pretium fusce id velit ut tortor.
+			Porttitor rhoncus dolor purus non enim praesent elementum. Malesuada pellentesque elit eget gravida
+			cum sociis natoque.<br>Ornare aenean euismod elementum nisi quis eleifend quam adipiscing. Purus in
+			mollis nunc sed id semper. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut
+			placerat. Tempor orci eu lobortis elementum nibh tellus molestie nunc. Sed vulputate mi sit amet
+			mauris commodo. Nec ullamcorper sit amet risus nullam. Aenean euismod elementum nisi quis eleifend
+			quam. Morbi non arcu risus quis varius quam quisque. Est velit egestas dui id ornare arcu odio ut
+			sem. Pellentesque eu tincidunt tortor aliquam nulla facilisi cras fermentum odio. Nibh tortor id
+			aliquet lectus proin nibh nisl condimentum id. In hendrerit gravida rutrum quisque non tellus orci.
+			Vehicula ipsum a arcu cursus. Amet risus nullam eget felis eget nunc lobortis. Enim nunc faucibus
+			a pellentesque. Dignissim convallis aenean et tortor. Imperdiet proin fermentum leo vel orci porta
+			non pulvinar neque.</p>
+		<?php else: ?>
+			<p>Items Found:<?=$result_count;?></p>
+			<section class="items"> <!--problem with CSS. this element has backgroundcolor Tomato-->
+				<?php foreach($items as $item):?>
+					<div class="item-card">
+						<div class="item-image">
+							<img src="img/Logo_GroupOne.png"/>
+						</div>
+						<div class="item-info">
+							<h1><?=$item['name'];?></h1>
+							<h2><?=$item['price'];?></h2>
+							<p><?=$item['description'];?><br></p>
+					</div>
+				</div>
+				<?php endforeach;?>
+			</section>
+		<?php endif;?>
+		</article>
 </body>
 <footer>
-<?php include 'footer.php';?>
+	<?php include 'footer.php';?>
 </footer>
 </html>
