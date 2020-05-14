76f56201657465f86952c2e8f8dd56b424aa1bf9
Gabriel ML <gabriellevineml@gmail.com>
2020-05-14 12:21:48 -0400

    made Create Item form into a sticky form (the image upload isn't sticky)

--- create_item.php
+++ create_item.php
@@ -29,27 +29,26 @@ mysqli_close($dbc);
 			<form action="handle_create_item.php" method="post" enctype="multipart/form-data">
 			<div class="">
 				<ul>
-					<li>Name: <input type="text" id="itemname" name="itemname"></li>
+					<li>Name: <input type="text" id="itemname" name="itemname" value="<?php if (isset($_REQUEST['itemname'])) { echo $_REQUEST['itemname']; } ?>"></li>
 					<li>Image: <input type='file' name='imageFile'>
 					<!--<li>Description: <input type="text" id="itemdescription" name="itemdescription"></li>-->
-					<li>Description:<textarea id ="itemdescription" name="itemdescription" rows=4 cols="40">
-					</textarea></li>
+					<li>Description:<textarea id ="itemdescription" name="itemdescription" rows=4 cols="40" ><?php if (isset($_REQUEST['itemdescription'])) { echo $_REQUEST['itemdescription']; } ?></textarea></li>
 					<li><label for="systemselecter">System</label>
 					<select  name="systemselecter" id="systemselecter">
 						<?php foreach($systems as $systems):?>
-							<option value="<?= $systems['system_intials']?>"><?= $systems['system_name']?></option>
+							<option value="<?= $systems['system_intials']?>" <?php if (isset($_REQUEST['systemselecter'])) { if ($systems['system_intials'] == $_REQUEST['systemselecter']) { echo 'selected="selected"'; }} ?> ><?= $systems['system_name']?></option>
 						<?php endforeach;?>
 					</select>
 					</li>
 					<li><label for="genreselecter">Genre</label>
 					<select  name="genreselecter" id="genreselecter">
 						<?php foreach($genres as $genres):?>
-							<option value="<?= $genres['genre_name']?>"><?= $genres['genre_name']?></option>
+							<option value="<?= $genres['genre_name']?>" <?php if (isset($_REQUEST['genreselecter'])) { if ($genres['genre_name'] == $_REQUEST['genreselecter']) { echo 'selected="selected"'; }} ?> ><?= $genres['genre_name']?></option>
 						<?php endforeach;?>
 					</select>
 					</li>
 					<li><label for="priceslider">Set Price</label>
-					<input type="range" name="priceslider" id="priceslider" min="0" max="500" value="50">
+					<input type="range" name="priceslider" id="priceslider" min="0" max="500" value="<?php if (isset($_REQUEST['priceslider'])) { echo $_REQUEST['priceslider']; } else { echo 50; } ?>">
 					<br><p>$<span id="currentprice"></span></p></li>
 					<script src="scripts.js"></script>
 				</ul>
--- handle_create_item.php
+++ handle_create_item.php
@@ -1,22 +1,17 @@
 <?php
-require_once 'database_connection.php';
-$dbc  = connect_to_database();
-//if (!item_exists($dbc))
-//{
-	create_item($dbc);
-//}
 include('create_item.php');
 
-//function item_exists($dbc) {
-//
-//	$query = "SELECT * from items WHERE name='" . $_REQUEST['itemname'];
-//	$result = mysqli_query($dbc,$query);
-//	if (mysql_affected_rows($result) > 0)
-//	{
-//		echo "Item already exists!";
-//	}
-//	return (mysql_affected_rows($result) > 0);
-//}
+require_once 'database_connection.php';
+$dbc  = connect_to_database();
+create_item($dbc);
+
+
+/*function item_exists($dbc) {
+
+	$query = "SELECT * from items WHERE name='" . $_REQUEST['itemname'];
+	$result = mysqli_query($dbc,$query);
+	return (mysql_affected_rows($result) > 0);
+}*/
 
 function create_item($dbc) {
 	$name = $_REQUEST['itemname'];
@@ -24,44 +19,52 @@ function create_item($dbc) {
 	$price = $_REQUEST['priceslider'];
 	$system = $_REQUEST['systemselecter'];
 
-	$file = $_FILES['imageFile'];
+	if ($name === '') {
+		echo "<script type='text/javascript'>alert('Item must have a name!');</script>";
+	}
+	else if ($description === '') {
+		echo "<script type='text/javascript'>alert('Item must have a description!');</script>";
+	}
+	else
+	{
 
-	$fileName = $_FILES['imageFile']['name'];
-	$fileTempName = $_FILES['imageFile']['tmp_name'];
-	$fileSize = $_FILES['imageFile']['size'];
-	$fileError = $_FILES['imageFile']['error'];
-	$fileType = $_FILES['imageFile']['type'];
+		$file = $_FILES['imageFile'];
 
-	$fileExtension = explode('.', $fileName);
-	$fileActualExtension = strtolower(end($fileExtension));
+		$fileName = $_FILES['imageFile']['name'];
+		
+		$fileTempName = $_FILES['imageFile']['tmp_name'];
+		$fileSize = $_FILES['imageFile']['size'];
+		$fileError = $_FILES['imageFile']['error'];
+		$fileType = $_FILES['imageFile']['type'];
 
-	$allowedExtensions = array('jpg', 'jpeg', 'png');
+		$fileExtension = explode('.', $fileName);
+		$fileActualExtension = strtolower(end($fileExtension));
+
+		$allowedExtensions = array('jpg', 'jpeg', 'png');
 	
-	if (in_array($fileActualExtension, $allowedExtensions)) {
-		if ($fileError === 0) {
-			if ($fileSize < 1000000) {
-				$imageName = $system . "_" . $name . "." . $fileActualExtension;
-				move_uploaded_file($fileTempName, 'img/' . $system . '/' . $imageName);
+		if (in_array($fileActualExtension, $allowedExtensions)) {
+			if ($fileError === 0) {
+				if ($fileSize < 1000000) {
+					$imageName = $system . "_" . $name . "." . $fileActualExtension;
+					move_uploaded_file($fileTempName, 'img/' . $system . '/' . $imageName);
 
-				$query = "INSERT INTO items VALUES(NULL,'" . $name . "','" . $imageName . "','" . $description . "','" . $price . "','" . $system . "','" . "action" . "',NOW());";
-				echo $query . "<br/>";
-				$result = mysqli_query($dbc,$query);
-				if($result){
-					echo "*New item created!" . 'img/' . $system . '/' . $imageName;
-				}else{
-					echo "*There was an issue creating the item!";
+					$query = "INSERT INTO items VALUES(NULL,'" . $name . "','" . $imageName . "','" . $description . "','" . $price . "','" . $system . "','" . "action" . "',NOW());";
+					echo $query . "<br/>";
+					$result = mysqli_query($dbc,$query);
+					if($result) {
+						echo "<script type='text/javascript'>alert('New item created!');</script>";
+					} else {
+						echo "<script type='text/javascript'>alert('There was an issue creating the item!');</script>";
+					}
+				} else {
+					echo "<script type='text/javascript'>alert('Image to big to upload!');</script>";
 				}
 			} else {
-				echo "*Image to big to upload";
-				return;
+				echo "<script type='text/javascript'>alert('Ran into an issue uploading the image!');</script>";
 			}
 		} else {
-			echo "*Ran into an issue uploading the image";
-			return;
+			echo "<script type='text/javascript'>alert('This file type is not supported!');</script>";
 		}
-	} else {
-		echo "*This file type is not supported.";
-		return;
 	}
 }
 ?>
