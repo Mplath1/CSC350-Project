2e956183a1edab825645e27d49cea088c3b61a1d
Gabriel ML <gabriellevineml@gmail.com>
2020-05-14 15:06:44 -0400

    more changes to login css

--- login.php
+++ login.php
@@ -16,25 +16,26 @@ if(!isset($msg)){$msg="";}
     form.submit();
   }
 </script>
-<div class="genericPanelRed" style="position:absolute;top:30%;left:45%;">
-<form id="test" action="handle_login.php" method="post">
-  <div style="padding: 6px">
-	<label for="username">Username:</label>
-	<input type="text" name="username"><br>
+<div class="genericPanelRed" style="width:256px; position:absolute;top:45%;left:50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%);">
+	<h1 style="text-align: center;">Login!</h1>
+	<form id="test" action="handle_login.php" method="post">
+		<div style="padding: 6px; text-align:right;">
+			<label for="username">Username:</label>
+			<input type="text" name="username"><br>
+		</div>
+		<div style="padding: 6px; text-align:right;">
+			<label for="password">Password:</label>
+			<input type="password" name="password"><br>
+		</div>
+		<div style="padding: 6px;display: flex;justify-content:space-evenly;">
+			<!--possibly add image on button. mushroom for login, 1-up shroom for new user-->
+			<button type="submit" class="button" onclick="submitForm('handle_login.php')">Enter</button>
+			<button type="submit" onclick="submitForm('handle_new_user.php')">New User</button>
+		</div>
+		<div style="padding: 6px; display:flex;justify-content:center;">
+			<?php echo $msg;?>
+		</div>
+	</form>
 </div>
-<div style="padding: 6px">
-	<label for="password">Password:</label>
-	<input type="password" name="password"><br>
-</div>
-<div style="padding: 6px;display: flex;justify-content:space-evenly;">
-  <!--possibly add image on button. mushroom for login, 1-up shroom for new user-->
-	<button type="submit" onclick="submitForm('handle_login.php')">Enter</button>
-	<button type="submit" onclick="submitForm('handle_new_user.php')">New User</button>
-</div>
-<div style="padding: 6px; display:flex;justify-content:center;">
-<?php echo $msg;?>
-</div>
-</div>
-</form>
 </body>
 </html>
