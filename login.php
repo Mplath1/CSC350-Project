b5173e6380ebecb9db917286a7cbd36d0e6af518
Michael Plath <mikeaplath@gmail.com>
2020-05-14 14:39:52 -0400

    basic inline css on login.php

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if(!isset($msg)){$msg="";}
 <title>Login</title>
 </head>
 <body>
-<!--add css external file here. possibly add class divisions to style-->
+  <link rel="stylesheet" type="text/css" href="index_style.css">
 <script type="text/javascript">
   function submitForm(action) {
     var form = document.getElementById('test');
@@ -16,14 +16,25 @@ if(!isset($msg)){$msg="";}
     form.submit();
   }
 </script>
+<div class="genericPanelRed" style="position:absolute;top:30%;left:45%;">
 <form id="test" action="handle_login.php" method="post">
+  <div style="padding: 6px">
 	<label for="username">Username:</label>
 	<input type="text" name="username"><br>
+</div>
+<div style="padding: 6px">
 	<label for="password">Password:</label>
 	<input type="password" name="password"><br>
+</div>
+<div style="padding: 6px;display: flex;justify-content:space-evenly;">
+  <!--possibly add image on button. mushroom for login, 1-up shroom for new user-->
 	<button type="submit" onclick="submitForm('handle_login.php')">Enter</button>
 	<button type="submit" onclick="submitForm('handle_new_user.php')">New User</button>
-	<?php echo $msg;?>
+</div>
+<div style="padding: 6px; display:flex;justify-content:center;">
+<?php echo $msg;?>
+</div>
+</div>
 </form>
 </body>
 </html>
